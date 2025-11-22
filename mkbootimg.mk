85cb1490e828b81da4e124bb2ed049478c8bb6f9
Jamie Falconer <mrfluffyoven@gmail.com>
2025-11-22 14:21:06 +0000

    Update MKBOOTIMG path for Samsung A12s

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-MKBOOTIMG := device/samsung/a13/mkbootimg
+MKBOOTIMG := device/samsung/a12s/mkbootimg
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
