06e68e9a31bc9882a3edd84e6746ad722371fd55
mrfluffyoven <mrfluffyoven@gmail.com>
2025-12-19 12:05:56 +0000

    support orangefox
    Change-Id: Ia05ef1530c9b1f76ce5856973b819fd9ed321eb3

---
+++ fox_a12s.mk
@@ -0,0 +1,48 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2025 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+# screen settings
+OF_SCREEN_H := 2400
+OF_STATUS_H := 122
+OF_STATUS_INDENT_LEFT := 80
+OF_STATUS_INDENT_RIGHT := 80
+OF_USE_GREEN_LED := 0
+OF_HIDE_NOTCH := 1
+OF_ALLOW_DISABLE_NAVBAR := 0
+
+# number of list options before scrollbar creation
+OF_OPTIONS_LIST_NUM := 12
+
+# others
+OF_UNBIND_SDCARD_F2FS := 1
+OF_QUICK_BACKUP_LIST := /boot;/data;/dtbo;
+OF_PATCH_AVB20 := 1
+OF_USE_DMCTL := 1
+OF_ENABLE_ALL_PARTITION_TOOLS := 1
+
+# disable the flashlight
+OF_FLASHLIGHT_ENABLE := 0
+
+# FRP
+OF_ENABLE_FRP_ADDON := 1
+
+# battery percent
+OF_USE_LEGACY_BATTERY_SERVICES := 1
+#
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,69 @@
-export ALLOW_MISSING_DEPENDENCIES=true
-sudo chmod +rwx out/host/linux-x86/bin/avbtool
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2025 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+#set -o xtrace
+FDEVICE="a12s"
+
+fox_get_target_device() {
+local chkdev
+  if echo "$BASH_SOURCE" | grep -q "/$FDEVICE/"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif set | grep BASH_ARGV | grep -w \"$FDEVICE\"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif echo "${BASH_SOURCE[0]}" | grep -q "/$FDEVICE/"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif echo "$0" | grep -q "$FDEVICE"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  fi
+}
+
+if [ -z "$FOX_BUILD_DEVICE" ]; then
+	fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export TW_DEFAULT_LANGUAGE="en"
+	export LC_ALL="C"
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export FOX_VANILLA_BUILD=1
+	export FOX_NO_SAMSUNG_SPECIAL=1
+	export FOX_ENABLE_APP_MANAGER=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_USE_LZ4_BINARY=1
+	export FOX_USE_ZSTD_BINARY=1
+	export FOX_USE_DATE_BINARY=1
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v30.6.zip
+	export FOX_DELETE_INITD_ADDON=1
+	export FOX_DELETE_AROMAFM=1
+	export FOX_USE_BUSYBOX_BINARY=1
+
+else
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+	fi
+fi
+#
+
 chmod a+x device/samsung/a12s/mkbootimg
