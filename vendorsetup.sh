3ef6c9b54ff7965489a02f3459871bbd9356b56a
Jamie Leigh Falconer <mrfluffyoven@gmail.com>
2026-08-14 09:00:37 +0000

    try to fix adb sideload and reboot logic
    Change-Id: I0afb73b99ea0090e6044b86a29ed9c9f6c7ffd7b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,7 +16,7 @@
 #	See <http://www.gnu.org/licenses/>.
 #
 # 	Please maintain this if you use this script or any part of it
-#
+# 
 
 #set -o xtrace
 FDEVICE="a12s"
@@ -44,7 +44,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export FOX_VANILLA_BUILD=1
 	export FOX_NO_SAMSUNG_SPECIAL=1
-	export FOX_ENABLE_APP_MANAGER=1
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
 	export FOX_USE_TAR_BINARY=1
